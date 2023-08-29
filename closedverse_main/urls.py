b80523e7b5cb8c20e0616a1ac6554e79ac80e0a7
some weird guy <120821766+Mistake35@users.noreply.github.com>
2023-08-29 12:03:29 -0700

    god damnit

--- closedverse_main/urls.py
+++ closedverse_main/urls.py
@@ -113,4 +113,4 @@ urlpatterns = [
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # serve static and media i think???? mighTTT???????
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
+urlpatterns += [re_path(r'^i/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
